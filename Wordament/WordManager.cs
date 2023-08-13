8f0b0c79d89f6d3446793ba390bdf085adab16fc
Aptivi CEO <eoflaoevicecity@gmail.com>
2023-08-13 18:55:59 +0300

    add - Added the async version of functions
    We've added asynchronous functions.
    
    ---
    
    We've added asynchronous version of the functions so that web developers can use this library.
    
    ---
    
    Type: add
    Breaking: False
    Doc Required: False
    Part: 1/1

--- Wordament/WordManager.cs
+++ Wordament/WordManager.cs
@@ -27,6 +27,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace Wordament
 {
@@ -56,6 +57,32 @@ namespace Wordament
         }
 
         /// <summary>
+        /// Initializes the words. Does nothing if already downloaded.
+        /// </summary>
+        public static async Task InitializeWordsAsync()
+        {
+            // Download the words
+            if (Words.Count == 0)
+            {
+                var response = await httpClient.GetAsync("https://cdn.jsdelivr.net/gh/dwyl/english-words/words_alpha.txt");
+                var contentStream = new MemoryStream();
+                var stream = await response.Content.ReadAsStreamAsync();
+                stream.CopyTo(contentStream);
+                contentStream.Seek(0L, SeekOrigin.Begin);
+                Words.AddRange(new StreamReader(contentStream).ReadToEnd().SplitNewLines().ToList());
+            }
+        }
+
+        /// <summary>
+        /// Gets all words
+        /// </summary>
+        public static string[] GetWords()
+        {
+            InitializeWords();
+            return Words.ToArray();
+        }
+
+        /// <summary>
         /// Gets a random word
         /// </summary>
         /// <returns>A random word</returns>
@@ -77,6 +104,53 @@ namespace Wordament
         {
             // Get an initial word
             string word = GetRandomWord();
+            bool lengthCheck = wordMaxLength > 0;
+            bool startsCheck = !string.IsNullOrWhiteSpace(wordStartsWith);
+            bool endsCheck = !string.IsNullOrWhiteSpace(wordEndsWith);
+            bool exactLengthCheck = wordExactLength > 0;
+
+            // Loop until all the conditions that need to be checked are satisfied
+            while (!(((lengthCheck && word.Length <= wordMaxLength) || !lengthCheck) &&
+                     ((startsCheck && word.StartsWith(wordStartsWith)) || !startsCheck) &&
+                     ((endsCheck && word.EndsWith(wordEndsWith)) || !endsCheck) &&
+                     ((exactLengthCheck && word.Length == wordExactLength) || !exactLengthCheck)))
+                word = GetRandomWord();
+
+            // Get a word that satisfies all the conditions
+            return word;
+        }
+
+        /// <summary>
+        /// Gets all words
+        /// </summary>
+        public static async Task<string[]> GetWordsAsync()
+        {
+            await InitializeWordsAsync();
+            return Words.ToArray();
+        }
+
+        /// <summary>
+        /// Gets a random word
+        /// </summary>
+        /// <returns>A random word</returns>
+        public static async Task<string> GetRandomWordAsync()
+        {
+            await InitializeWordsAsync();
+            return Words[rng.Next(Words.Count)];
+        }
+
+        /// <summary>
+        /// Gets a random word conditionally
+        /// </summary>
+        /// <param name="wordMaxLength">The maximum length of the word</param>
+        /// <param name="wordStartsWith">The word starts with...</param>
+        /// <param name="wordEndsWith">The word ends with...</param>
+        /// <param name="wordExactLength">The exact length of the word</param>
+        /// <returns>A random word</returns>
+        public static async Task<string> GetRandomWordConditionalAsync(int wordMaxLength, string wordStartsWith, string wordEndsWith, int wordExactLength)
+        {
+            // Get an initial word
+            string word = await GetRandomWordAsync();
             bool lengthCheck = wordMaxLength > 0;
             bool startsCheck = !string.IsNullOrWhiteSpace(wordStartsWith);
             bool endsCheck = !string.IsNullOrWhiteSpace(wordEndsWith);
